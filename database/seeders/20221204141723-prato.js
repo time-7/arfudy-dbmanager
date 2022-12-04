7ce0e834759bd429313a138723bc75024f8856bc
gabrielznegri <gabrielznegri@gmail.com>
2022-12-04 18:57:29 -0300

    Adding images on prato seegers

--- database/seeders/20221204141723-prato.js
+++ database/seeders/20221204141723-prato.js
@@ -6,7 +6,7 @@ module.exports = {
                 {
                     ds_prato: 'Descrição teste 01',
                     nome: 'Macarronada ao molho sugo abolonhesado',
-                    imagem: 'Imagem teste 01',
+                    imagem: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fclaudia.abril.com.br%2Fgastronomia%2F30-receitas-macarrao%2F&psig=AOvVaw0G1k9EbX-yKK6ofLLvJ-Ya&ust=1670277284041000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLCb3vP54PsCFQAAAAAdAAAAABAW',
                     has3d: true,
                     valor: 10.1,
                     valor_nutricional: 20.2,
@@ -16,7 +16,7 @@ module.exports = {
                 {
                     ds_prato: 'Descrição teste 02',
                     nome: 'Famosa panqueca da utf (lendária)',
-                    imagem: 'Imagem teste 02',
+                    imagem: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.rbth.com%2Freceitas%2F85091-blini-com-carne-picadinha-receita&psig=AOvVaw1K3lhOo2X1DlXuYWL_c29x&ust=1670277360400000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCND0r5j64PsCFQAAAAAdAAAAABAD',
                     has3d: true,
                     valor: 11.11,
                     valor_nutricional: 22.22,
